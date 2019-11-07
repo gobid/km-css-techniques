fedf01f1fe9beced8058dfb4c936aa820680d259
daniel-xinyuan-zhu <danielzhu777@gmail.com>
2019-11-07 20:12:51 +0000

    Adding Salome's Changes for Part 1

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+	path = require('path'),
+	bodyParser = require("body-parser"),
+	fs = require('fs');
+
+app.use(express.static("approach1"));
+app.use(express.static(__dirname + 'views'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.get("/", function(req,res)
 {
-    res.send("landing");
+    res.send("Landing page");
 });
 
-app.get("/getCSS", function(req,res)
+app.get("/fPart1", function(req,res)
 {
-    res.send("getting CSS");
+    res.render('./index');
+});
+
+app.post("/visualOutcomes",function(req, res){
+	fs.writeFile('message.csv', req.body.visual_outcome, (err) => {
+  	if (err) throw err;
+	  console.log('The file has been saved!');
+	});
+	res.redirect("/fPart2");
+});
+
+app.get("/fPart2", function(req,res)
+{
+	var dataArray = [];
+	fs.readFile('message.csv', 'utf8', function (err, data) {
+  		dataArray = data.split(/\r?\n/);
+	  	console.log(dataArray);
+		res.render('./code', {visualOutcomes: dataArray});
+	});
 });
 
 app.listen(4000, process.env.IP, function(){
---
+++ approach1/local.js
@@ -0,0 +1,48 @@
+// import { saveAs } from 'file-saver';
+var slider = document.getElementById("myRange");
+var pageWidth = document.getElementById("pageWidth");
+pageWidth.innerHTML = slider.value/ parseFloat(getComputedStyle(document.querySelector('#articles_page'))['font-size']) +" em"; // Display the default slider value
+var num_visual_outcomes = 0;
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    pageWidth.innerHTML = slider.value/ parseFloat(getComputedStyle(document.querySelector('#articles_page'))['font-size'])+" em";
+    document.getElementById("articles_page").style.width = slider.value + "px";
+	var iframe = document.getElementById('articles_page');
+	// console.log(iframe.contentWindow);
+	var feed_wrap = iframe.contentDocument.getElementsByClassName("blog-feed__wrap")[0]
+	console.log(feed_wrap);
+	console.log(window.getComputedStyle(feed_wrap).getPropertyValue('grid-template-columns'));
+}
+
+function addVisualOutcome(){
+    var input = document.createElement("input");
+    input.classList.add("form-control");
+	input.name = "visual_outcome";
+	input.type = "text";
+	input.placeholder = "new visual observation"
+    document.getElementById("outcomes").appendChild(input);
+    num_visual_outcomes += 1;
+}
+
+function updateDropdown() { 
+	selectElement =  
+			document.querySelector('#outcome_dropdown'); 
+
+	output = selectElement.value;
+	console.log(output);
+
+	document.querySelector('#outcome_description').innerHTML = "<%= visualOutcomes["+selectElement.value + "] %>";
+	// 		= output; 
+}
+
+function populateEditor(){
+    // for(var i=0; i<num_visual_outcomes; i++){
+
+    // }
+    // var dropdown = document.getElementById("outcome_dropdown");
+    console.log("new file");
+    var blob = new Blob(["Welcome to Websparrow.org."],
+                { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "static.txt");
+}
