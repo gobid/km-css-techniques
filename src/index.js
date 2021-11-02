1dc7584068448c84040102808d8c97c46bd2f584
Fardeem Munir <munirfardeem@gmail.com>
2021-11-02 04:48:56 -0500

    More clean up

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// import App from "./App";
-import "./index.css";
-// import InteractiveApp from "./InteractiveApp";
-import NewApp from "./NewApp";
-
-ReactDOM.render(<NewApp />, document.getElementById("root"));
