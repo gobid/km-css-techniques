367d2c293ea55f2db8860f78e8f5d63c6327bbc3
Fardeem Munir <munirfardeem@gmail.com>
2021-11-11 16:36:10 -0600

    Fix editor

--- src/components/CSSEditor.tsx
+++ src/components/CSSEditor.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { Formik, Form, FieldArray, Field, useFormikContext } from "formik";
 import { Declaration } from "../lib/types";
