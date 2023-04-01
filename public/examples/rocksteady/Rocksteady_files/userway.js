33e6c031ad2459066f0287e75355ee5abda8f61c
Gobi Dasu <gobi@gobidasu.com>
2023-04-01 11:57:56 -0500

    Add pre post test examples

---
+++ public/examples/rocksteady/Rocksteady_files/userway.js
@@ -0,0 +1,9 @@
+;(function userway() {
+  const s = document.createElement('script')
+  s.setAttribute('data-account', 'eS2ZymO5OB')
+  s.setAttribute('src', 'https://cdn.userway.org/widget.js')
+  const appendToBody = () => {
+    document.body.appendChild(s)
+  }
+  document.addEventListener('DOMContentLoaded', appendToBody)
+})()
