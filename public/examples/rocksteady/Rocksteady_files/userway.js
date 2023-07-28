acb947d872f7407beb7a45b778d46cd80f4ebabe
Gobi Dasu <gobi@gobidasu.com>
2023-07-28 09:12:13 -0500

    update rocksteady

--- public/examples/rocksteady/Rocksteady_files/userway.js
+++
@@ -1,9 +0,0 @@
-;(function userway() {
-  const s = document.createElement('script')
-  s.setAttribute('data-account', 'eS2ZymO5OB')
-  s.setAttribute('src', 'https://cdn.userway.org/widget.js')
-  const appendToBody = () => {
-    document.body.appendChild(s)
-  }
-  document.addEventListener('DOMContentLoaded', appendToBody)
-})()
